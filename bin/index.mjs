6911e2bcaba149483bc2a01f578a0af8a60b8382
Clay Miller <clay@smockle.com>
2018-02-05 20:12:46 -0800

    Improve CLI. Add 'withoutTitles'

--- bin/index.mjs
+++ bin/index.mjs
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 import { liturgicalCalendar } from "../lib";
 
-const calendar = liturgicalCalendar();
+const year = process.argv.slice(2)[0]
+  ? parseInt(process.argv.slice(2)[0], 10)
+  : null;
+const calendar = liturgicalCalendar(year);
 console.log(calendar);
